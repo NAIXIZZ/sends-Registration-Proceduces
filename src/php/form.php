6fc96f1e9e15b0cc6144f4fc69f3b3504ec37d14
unknown <2896192354@qq.com>
2021-08-01 11:55:01 +0800

    update

--- src/php/form.php
+++ src/php/form.php
@@ -1,43 +1,27 @@
 <?php
-    header('Content-type:text/html; charset=utf-8');
-    session_start();
-    $link = mysqli_connect('localhost', 'root', '123456', 'stu');
-    mysqli_set_charset($link, 'utf8');
-    //通过表单获取用户名和密码，连接数据库后用mysqli_query查询userinfo
-    $result = mysqli_query($link, 'SELECT * FROM `userinfo`');
-    $flag = true;
-    while ($row = mysqli_fetch_assoc($result)) {
-        $username = $row['u_name'];
-        $password = $row['u_password'];
-
-        if (isset($_POST['login'])) {
-            if (($_POST['username'] == '') || ($_POST['password'] == '')) {
-                // 若有一项为空,视为未填写,提示输入
-                header('url=signin.php');
-                echo "<div class = \"info\">请输入...</div>";
-                exit;
-            } elseif (($_POST['username'] != $username) || ($_POST['password'] != $password)) {
-                // 用户名或密码错误
-                header('url=signin.php');
-                // echo "<div class = \"info\">用户名或密码错误,请重新填写登录信息!</div>";
-                $flag = false;
-                continue;
-            } elseif (($_POST['username'] = $username) && ($_POST['password'] = $password)) {
-                $_SESSION['username'] = $_POST['username'];
-                $_SESSION['password'] = $_POST['password'];
-                $_SESSION['usertype'] = $row['u_type'];
-                //用session保存登录信息，以防后面用到
-                if ($_SESSION['usertype'] == "A") {
-                    header('location:admin.php');
-                } elseif ($_SESSION['usertype'] == "T") {
-                    header('location:teacher.php');
-                } elseif ($_SESSION['usertype'] == "S") {
-                    header('location:student.php');
-                }
-            }
-        }
+// 连接数据库、设置字符集
+$link = @mysqli_connect('localhost', 'root', '123456', 'information')
+    or die("无法连接到服务器");
+mysqli_set_charset($link, 'utf8');
+// 获取查询结果集
+if (!$_POST['name'] == '' ) {
+    $name = $_POST['name'];
+    $id = $_POST['id'];
+    $phone = $_POST['phone'];
+    $qq = $_POST['qq'];
+    $campus = $_POST['campus'];
+    $academy = $_POST['academy'];
+    $profession = $_POST['profession'];
+    $first = $_POST['firstVolunteer'];
+    $second = $_POST['secondVolunteer'];
+    $sq_add = "INSERT INTO information(s_name,id,phone,qq,campus,academy,profession,firstVolunteer,secondVolunteer)
+        VALUES('$name','$id','$phone','$qq','$campus','$academy','$profession','$first','$second')";
+    if (!mysqli_query($link, $sq_add)) {
+        echo "<h1>提交失败</h>";
+    } else {
+        echo "<h1>报名成功</h>";
     }
-    if ($flag == false) {
-        echo "<div class = \"info\">用户名或密码错误,请重新填写登录信息!</div>";
-    }
-    ?>
+    mysqli_close($link);
+} else {
+    echo "<p class=\"tips\">请输入要添加的内容(添加内容不可为空)</p>";
+}
